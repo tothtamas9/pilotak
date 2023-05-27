4f4200abc7b5ab54463ccdca7e558d137f5e5374
ttama <ttama@TOMSZ-LAPTOP>
2023-05-27 18:49:48 +0200

    Add project files.

---
+++ pilotak/PilotakAdatai.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pilotak
+{
+    public class PilotakAdatai
+    {
+        string nev;
+        DateTime szuletesi_datum;
+        string nemzetiseg;
+        int rajtszam;
+
+        public PilotakAdatai(string nev, DateTime szuletesi_datum, string nemzetiseg, int rajtszam)
+        {
+            this.nev = nev;
+            this.szuletesi_datum = szuletesi_datum;
+            this.nemzetiseg = nemzetiseg;
+            this.rajtszam = rajtszam;
+        }
+
+        public string Nev { get => nev; }
+        public DateTime Szuletesi_datum { get => szuletesi_datum; }
+        public string Nemzetiseg { get => nemzetiseg; }
+        public int Rajtszam { get => rajtszam; }
+    }
+}
---
+++ pilotak/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace pilotak
+{
+    public class Program
+    {
+        static List<string> mindenAdat;
+        static List<string> pilotaNevek;
+
+        static void Main(string[] args)
+        {
+            mindenAdat = File.ReadAllLines("Adatok\\pilotak.csv").Skip(1).ToList();
+
+            pilotaNevek = File.ReadAllLines("Adatok\\pilotak.csv").Skip(1).Select(sor => sor.Split(';')[0]).ToList();
+
+            Console.WriteLine($"3. feladat: {mindenAdat.Count}");
+
+            Console.WriteLine($"4. feladat: {pilotaNevek.Last()}");
+        }
+    }
+}
